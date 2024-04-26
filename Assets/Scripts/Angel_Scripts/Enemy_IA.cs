986dcbeedafc46ffab8857989f08c1ae6bb9428b
Jared Osorno <jaredlares5@gmail.com>
2024-04-26 13:37:46 -0600

    primera decision del arbol

--- Assets/Scripts/Angel_Scripts/Enemy_IA.cs
+++ Assets/Scripts/Angel_Scripts/Enemy_IA.cs
@@ -1,21 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class Enemy_IA : MonoBehaviour
 {
-    int choice = 0;
-    void Start()
-    {
-        //Acceso al wave manager
-        //WaveManager.instance
-    }
-
+    // cosas nesesarias
+    [SerializeField] private List<GameObject> spawnPoints;
+    [SerializeField]private int ammount = 0;
+    [SerializeField]private int choiceLine = 0;
+    [SerializeField]private int LineAmount = 0;
+    [SerializeField] private const int totalLines = 5;
+    //Acceso al wave manager
+    //WaveManager.instance
+    // economi.instance.money
     void Update()
     {
-        choice = Choice();
-        switch (choice)
+        switch (Choice())
         {
             case 1:
                 PlaceTorret();
@@ -32,14 +34,55 @@ public class Enemy_IA : MonoBehaviour
         }
     }
 
+    //calculed the enemies total amoundt
+    int totalAmount()
+    {
+        int amount = 0;
+        for (int i = 0; i < totalLines; i++)
+        {
+            amount += WaveManager.instance.enemies[i].totalAmount;
+        }
+        return amount;
+    }
+    void lineMoreAmount()
+    {
+        for (int i = 0; i < totalLines; i++)
+        {
+            if (i == 0)
+            {
+                choiceLine = i;
+                LineAmount = WaveManager.instance.enemies[i].totalAmount;
+            }
+            else if(LineAmount < WaveManager.instance.enemies[i].totalAmount)
+            {
+                choiceLine = i;
+                LineAmount = WaveManager.instance.enemies[i].totalAmount;
+            }
+        }
+    }
     // here will choice whats gonna do
     int Choice()
     {
+        ammount = totalAmount();
+        if (ammount > 2)
+        {
+           if(LineAmount > 2 )
+            {
+                return 2;
+            }
+        }
+        else
+        {
+            if(true)
+            {
+
+            }
+        }
         int auxChoice = 0;
         return auxChoice;
     }
 
-
+    #region actions
     // here will place the torret
     void PlaceTorret()
     {
@@ -56,4 +99,5 @@ public class Enemy_IA : MonoBehaviour
     {
 
     }
+    #endregion 
 }
